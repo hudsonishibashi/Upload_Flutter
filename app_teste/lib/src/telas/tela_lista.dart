daba4704d2307b82064f02e61f83eb312297f142
Hudson Ishibashi <ishibashi_90@hotmail.com>
2020-03-23 21:52:57 -0300

    'alteracaoTextoExcluirdaLista'

--- app_teste/lib/src/telas/tela_lista.dart
+++ app_teste/lib/src/telas/tela_lista.dart
@@ -140,7 +140,7 @@ class _ListPautaState extends State<ListPauta> {
         builder: (context){
      return AlertDialog(
          title: Text("Excluir?"),
-            content: Text("Tem certeza que deseja excluir?"),
+            content: Text("Tem certeza que deseja excluir esse item?"),
              actions: <Widget>[
                FlatButton(
                child: Text("Cancelar"),
